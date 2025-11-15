b48944ecb5c291ed004799d080283a143b62a8ad
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-11-16 01:33:26 +0200

    make a pagination and footer

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -17,7 +17,10 @@ export class HomeComponent implements OnInit {
   products: any = []
   allCategories: any = []
   term:string = ''
-  
+  paginatedProducts: any[] = [];
+  currentPage:number = 1
+  productsPerPage:number = 6;
+
   constructor(private _ProductsService: ProductsService){}
 
   ngOnInit(): void {
@@ -27,8 +30,46 @@ export class HomeComponent implements OnInit {
         console.log(response)
 
         this.allCategories = [...new Set(this.products.map((item: any) => item.category))];
+
+        this.updatePaginationView();
       }
     })
   }
 
+
+  updatePaginationView():void {
+    const startIndex = (this.currentPage - 1) * this.productsPerPage;
+    const endIndex = startIndex + this.productsPerPage;
+    this.paginatedProducts = this.products.slice( startIndex, endIndex )
+  }
+
+  getTotalPages():number {
+    return Math.ceil(this.products.length / this.productsPerPage)
+  }
+
+  getPageNumbers(): number[] {
+    const totalPages = this.getTotalPages();
+    return Array.from({length: totalPages}, (_,i) => i + 1)
+  }
+
+  goToPage(page: number) {
+    if(page < 1 || page > this.getTotalPages()) {
+      return;
+    }
+    this.currentPage = page;
+    this.updatePaginationView()
+  }
+
+  nextPage(): void {
+    if(this.currentPage < this.getTotalPages()) {
+      this.currentPage++;
+      this.updatePaginationView();
+    }
+  }
+  prevPage(): void {
+    if(this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginationView();
+    }
+  }
 }
