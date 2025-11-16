499e82e282e2b6b7280c15c50bd719357f34b948
ahmed Magdy <ahmedmagdee04@gmail.com>
2025-11-16 12:42:01 +0200

    finish products page

--- client/src/app/products/products.component.ts
+++ client/src/app/products/products.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { CommonModule } from '@angular/common';
+import { ProductitemComponent } from '../productitem/productitem.component';
+import { SearchPipe } from '../search.pipe';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProductitemComponent, SearchPipe, FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products: any = []
+  term:string = ''
+  paginatedProducts: any[] = [];
+  currentPage:number = 1
+  productsPerPage:number = 8;
+  constructor(private _ProductsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this._ProductsService.getAllProducts().subscribe({
+      next: (response) => {
+        this.products = response
+        this.updatePaginationView();
+      }
+
+    })
+  }
+
+updatePaginationView():void {
+    const startIndex = (this.currentPage - 1) * this.productsPerPage;
+    const endIndex = startIndex + this.productsPerPage;
+    this.paginatedProducts = this.products.slice( startIndex, endIndex )
+  }
+
+  getTotalPages():number {
+    return Math.ceil(this.products.length / this.productsPerPage)
+  }
+
+  getPageNumbers(): number[] {
+    const totalPages = this.getTotalPages();
+    return Array.from({length: totalPages}, (_,i) => i + 1)
+  }
+
+  goToPage(page: number) {
+    if(page < 1 || page > this.getTotalPages()) {
+      return;
+    }
+    this.currentPage = page;
+    this.updatePaginationView()
+  }
+
+  nextPage(): void {
+    if(this.currentPage < this.getTotalPages()) {
+      this.currentPage++;
+      this.updatePaginationView();
+    }
+  }
+  prevPage(): void {
+    if(this.currentPage > 1) {
+      this.currentPage--;
+      this.updatePaginationView();
+    }
+  }
+
+
 
 }
